ad895b1ee331507aa896e5b23938d409661abea2
Iwana Städeli <78117193+iwanast@users.noreply.github.com>
2022-03-22 14:16:05 +0100

    style item

--- shop-app/src/pages/Item.js
+++ shop-app/src/pages/Item.js
@@ -1,46 +1,44 @@
 import React, {useEffect, useState} from "react";
 import {Link, useLocation} from "react-router-dom";
 import { HiArrowSmLeft, HiArrowSmRight } from "react-icons/hi";
+import './Item.css';
 
 export const Item = () => {
-const location = useLocation()
-const item = location.state.product;
-const images = item.picture;
-console.log(images)
-const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
+  const location = useLocation()
+  const item = location.state.product;
+  const images = item.picture;
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-const nextPicture = () => {
- setCurrentImageIndex(currentImageIndex === images.length - 1 ? 0 : currentImageIndex+1) 
-
-};
-const prevPicture = () => {
-setCurrentImageIndex(currentImageIndex === 0 ? images.length - 1 : currentImageIndex -1);
-};
+  const nextPicture = () => {
+  setCurrentImageIndex(currentImageIndex === images.length - 1 ? 0 : currentImageIndex+1)
+  };
+  const prevPicture = () => {
+  setCurrentImageIndex(currentImageIndex === 0 ? images.length - 1 : currentImageIndex -1);
+  };
 
   return(
     <div> 
       <Link to={"/products"}>
-      <button>
+        <button>
           Back to Products
         </button>
       </Link>
-     <main id="main" className="outer-wrapper-product">
-          <div key={item._id} className="wrapper-product" id="item">
-              <img src={images[currentImageIndex]} alt="Book-cover" />
-              {/* <img src={item.picture.first} alt="Book-cover" />
-              <img src={item.picture.back} alt="Book-cover" /> */}
-              <div className="wrapper-button-picture-slide"><button onClick={prevPicture}><HiArrowSmLeft /></button>
-              <button onClick={nextPicture}><HiArrowSmRight /></button>
-              </div>
-              <div className="">
-                <span>{item.author}</span>
-                <h3>{item.title}</h3>
-                <p>{item.description}</p>
-                <p>{item.price} {item.currency}</p>
-              </div>
+     <main id="main">
+        <div key={item._id} className="wrapper-item">
+          <img src={images[currentImageIndex]} alt="Book-cover" />
+          <div className="wrapper-button-picture-slide">
+            <button onClick={prevPicture} className="button-item"><HiArrowSmLeft /></button>
+            <button onClick={nextPicture} className="button-item"><HiArrowSmRight /></button>
           </div>
-    </main>
+          <div>
+            <span>{item.author}</span>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <p>{item.price} {item.currency}</p>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
